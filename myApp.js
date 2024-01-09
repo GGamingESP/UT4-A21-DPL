c9e15b5e0e88d7526f6a78e86369801f4cd04d8a
Ivan <ivandasilvamartin1@gmail.com>
2024-01-09 19:15:58 +0000

    quinto

--- myApp.js
+++ myApp.js
@@ -3,11 +3,18 @@ let app = express();
 
 const absolutePath = __dirname + "/views/index.html"
 console.log("Hello World")
+let json = {
+    message: "Hello json"
+}
 
 app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", function(req, res) {
     res.sendFile(absolutePath);
+})
+
+app.get("/json", function(req, res) {
+    res.json(json);
 })
 
 
