79ce080fac2322e91e73faccbc279c37a979bf4c
Ivan <ivandasilvamartin1@gmail.com>
2024-01-09 18:59:36 +0000

    cuarto

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ let app = express();
 const absolutePath = __dirname + "/views/index.html"
 console.log("Hello World")
 
+app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", function(req, res) {
     res.sendFile(absolutePath);
