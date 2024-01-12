acad3b9a439bda05965bfd1d25b474bddec93b97
Ivan <ivandasilvamartin1@gmail.com>
2024-01-12 18:23:29 +0000

    a7

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ let app = express();
 const absolutePath = __dirname + "/views/index.html"
 console.log("Hello World")
 
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " " + req.ip);
+  next();
+})
+
 app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", function(req, res) {
@@ -15,7 +20,7 @@ app.get("/", function(req, res) {
 app.get("/json", function(req, res) {
     let message = 'Hello json';
 
-  // Check process.env.MESSAGE_STYLE and transform the message accordingly
+  // Comprueba el ENV y mira si la variable MESSAGE_STYLE existe y si el valor de esta es uppercase
     if (process.env.MESSAGE_STYLE && process.env.MESSAGE_STYLE.toLowerCase() === 'uppercase') {
         message = message.toUpperCase();
     }
